5a42d7443225ec516e3c684a5c46cc31a4ea7954
Evanski4 <evan.oconnor12344@gmail.com>
2023-10-12 19:51:50 -0500

    MoveWASD

--- Assets/Scripts/MoveWASD.cs
+++ Assets/Scripts/MoveWASD.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class MoveWASD : MonoBehaviour
 {
+    public float Speed;
+
+    float MovementX;
+    float MovementY;
+
+    Rigidbody2D Rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Rb = GetComponent<Rigidbody2D>();
+        MovementX = 0;
+        MovementY = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Rb.velocity = new Vector2(MovementX * Speed * Time.deltaTime, MovementY * Speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            MovementY = 1;
+
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            MovementY = -1;
+
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            MovementX = -1;
+
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            MovementX = 1;
+
+        }
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S))
+        {
+            MovementY = 0;
+
+        }
+        if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
+        {
+            MovementX = 0;
+
+        }
     }
 }
