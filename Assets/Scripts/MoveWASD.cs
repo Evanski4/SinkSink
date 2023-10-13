906f90dc26f6ebe7b7d93c5472cea113b5462e0a
Evanski4 <evan.oconnor12344@gmail.com>
2023-10-12 19:46:55 -0500

    ARROWKEYS

--- Assets/Scripts/MoveWASD.cs
+++ Assets/Scripts/MoveWASD.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class rigi : MonoBehaviour
+public class MoveWASD : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
