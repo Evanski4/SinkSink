84c698a5810a1e90a01291ed1a0565bd9cc9ae09
mewheniwhen <kwag8192@gmail.com>
2023-11-02 21:05:10 -0500

    Fish movement

--- Assets/Scripts/Part3/FishBehavior.cs
+++ Assets/Scripts/Part3/FishBehavior.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Fish : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
+    public int FishSpeed = 20;
+    public Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.velocity = transform.right * 20;
     }
 }
